0ca429f776efe017b7566c2bfa5d73dc9a576039
denniedeng <289447554@qq.com>
2018-10-20 13:30:53 +0800

    add netty server and client code

---
+++ src/main/java/xx/test/netty/server/NettyServer.java
@@ -0,0 +1,40 @@
+package xx.test.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 丢弃任何进入的数据 启动服务端的DiscardServerHandler
+ */
+public class NettyServer {
+private static final int portNumber = 7878;
+    
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new HelloServerInitializer());
+            
+
+            // 服务器绑定端口监听
+            ChannelFuture f = b.bind(portNumber).sync();
+            // 监听服务器关闭监听
+            f.channel().closeFuture().sync();
+
+            // 可以简写为
+            /* b.bind(portNumber).sync().channel().closeFuture().sync(); */
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
